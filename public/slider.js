601eb9186a62129b75ed383d86b910f89baeaced
AndrNikA <NikitaAndreev000@yandex.ru>
2021-12-26 02:41:38 +0300

    Добавлен массив для картинок

--- public/slider.js
+++ public/slider.js
@@ -1,3 +1,23 @@
+let image=[
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg"
+];
+let left=0;
+let sr=0;
+let right=0;
+let len=0;
+
+window.onload = function () {
+    len=image.length-1;
+    sr=Math.ceil(len/2);
+    document.querySelector(".left").src=image[sr-1];
+    document.querySelector(".center").src=image[sr];
+    document.querySelector(".right").src=image[sr+1];
+};
+
 let slider=document.querySelector(".slider");
 slider.addEventListener("click",function(event){ //События клика внутри блока слайдера
 	let el=event.target;
@@ -15,6 +35,13 @@ function sliderLeft(){ //Перемещение в лево
     block2.classList.add("left");
     block3.classList.remove("right");
     block3.classList.add("center");
+    if (sr==len) sr=0;
+    else sr+=1;
+    if (sr==len) right=0;
+    else right=sr+1;
+    if (sr==0) left=len;
+    else left=sr-1;
+    sliderSrc();
 }
 
 function sliderRight(){ //Перемещение в право
@@ -27,5 +54,18 @@ function sliderRight(){ //Перемещение в право
     block2.classList.add("right");
     block3.classList.remove("right");
     block3.classList.add("left");
+    if (sr==0) sr=len;
+    else sr-=1;
+    if (sr==0) left=len;
+    else left=sr-1;
+    if (sr==len) right=0;
+    else right=sr+1;
+    sliderSrc();
+}
+
+function sliderSrc(){
+    document.querySelector(".left").src=image[left];
+    document.querySelector(".center").src=image[sr];
+    document.querySelector(".right").src=image[right];
 }
 
