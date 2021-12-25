5ecac9c5ba1bcba4c9eb305c64ec750c67d86ac7
AndrNikA <NikitaAndreev000@yandex.ru>
2021-12-25 21:09:54 +0300

    Добавлены IMG и изменены HTML, CSS, JS

--- public/slider.js
+++ public/slider.js
@@ -1,18 +1,7 @@
-var block1=document.querySelector('#slider_block1');
-var block2=document.querySelector('#slider_block2');
-var block3=document.querySelector('#slider_block3');
-
-function funonload() {
-    block1.classList.add("left");
-    block2.classList.add("center");
-    block3.classList.add("right");
-} 
-window.onload = funonload;
-
-function left(){
-    block1=document.querySelector(".left");
-    block2=document.querySelector(".center");
-    block3=document.querySelector(".right");
+function sliderLeft(){
+    let block1=document.querySelector(".left");
+    let block2=document.querySelector(".center");
+    let block3=document.querySelector(".right");
     block1.classList.remove("left");
     block1.classList.add("right");
     block2.classList.remove("center");
@@ -21,10 +10,10 @@ function left(){
     block3.classList.add("center");
 }
 
-function right(){
-    block1=document.querySelector(".left");
-    block2=document.querySelector(".center");
-    block3=document.querySelector(".right");
+function sliderRight(){
+    let block1=document.querySelector(".left");
+    let block2=document.querySelector(".center");
+    let block3=document.querySelector(".right");
     block1.classList.remove("left");
     block1.classList.add("center");
     block2.classList.remove("center");
