563f3f737b1797de8c9846c2dcb42eeac69985d8
AndrNikA <NikitaAndreev000@yandex.ru>
2021-12-26 03:39:25 +0300

    Добавлено переключение через ленту

--- public/slider.js
+++ public/slider.js
@@ -18,17 +18,31 @@ window.onload = function () {
     document.querySelector(".left").src=image[sr-1];
     document.querySelector(".center").src=image[sr];
     document.querySelector(".right").src=image[sr+1];
-    for(let i=0;i<=len;i++) textcode+='<div class="slider_nav_b" id="slider_nav_b_'+i+'"></div>';
+    for(let i=0;i<=len;i++) textcode+='<div class="slider_nav_b" id="sliderNav_'+i+'"></div>';
     document.querySelector(".slider_nav").innerHTML=textcode;
     nav=document.querySelectorAll(".slider_nav_b");
     nav[sr].classList.add("slider_nav_b_hover");
 };
 
 let slider=document.querySelector(".slider");
-slider.addEventListener("click",function(event){ //События клика внутри блока слайдера
+slider.addEventListener("click",function(event){ 
 	let el=event.target;
     if (el.classList.contains('left')) sliderLeft();
     else if (el.classList.contains('right')) sliderRight();
+});
+
+let sliderNav=document.querySelector(".slider_nav");
+sliderNav.addEventListener("click",function(event){ 
+    let id=event.target.id;
+    let idz=id.length+1;
+    let num="";
+    for(let i=0;i<id.length;i++){
+        if(id[i]==="_") idz=i;
+        else if(i>idz) num+=id[i];
+    }
+    nav[sr].classList.remove("slider_nav_b_hover"); 
+    sr=parseInt(num);
+    sliderSrc();
 });
 
 function sliderLeft(){ //Перемещение в лево
@@ -41,7 +55,6 @@ function sliderLeft(){ //Перемещение в лево
     block2.classList.add("left");
     block3.classList.remove("right");
     block3.classList.add("center");
-
     nav[sr].classList.remove("slider_nav_b_hover");
     if (sr==len) sr=0;
     else sr+=1;
